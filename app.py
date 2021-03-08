d2fb632b54c7e971bc6abc3f6e8ea12f797fcd7b
Madhu Gandhe <gandhemadhu@gmail.com>
2021-03-08 20:12:49 +0530

    commited for multiple upload

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask, render_template
+import os
+#import magic
+import urllib.request
+
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 @app.route("/")
 def index():
    return render_template("index.html")
+
+@app.route("/sendfile", methods=["POST"])
+def send_file():
+   #  print(request.files)
+    print(request.files.getlist('files[]'))
+    for file in request.files.getlist('files[]'):
+       filename = secure_filename(file.filename)
+       file.save(os.path.join('D:\python_file_upload-45', filename))
+       print(file.filename)
+    #print(request.files)
+    return "successful_upload"
 
 if __name__ == '__main__':
    app.run(debug = True)
--- static/main.js
+++ static/main.js
@@ -1,31 +1,46 @@
+var form_data = new FormData();
+     // form_data.append("file[]",document.getElementById('multipleFile').files)
+      
 function readURL(input) {
     if (input.files && input.files[0]) {
-  
+     //dataList.push($('#fileData')[0]);
       var reader = new FileReader();
   
       reader.onload = function(e) {
-        $('.image-upload-wrap').hide();
   
         $('.file-upload-image').attr('src', e.target.result);
         $('.file-upload-content').show();
   
         // $('.image-title').html(input.files[0].name);
         $.each(input.files, function(index, value){
-            $(".image-title-wrap").append(value.name + '<button type="button" onclick="removeUpload()">Remove <span class="image-title">Uploaded Image</span></button> <br><br>');
+            form_data.append("files[]",document.getElementById('multipleFile').files[index]);
+            fileName = value.name.replace('.',"");
+            $(".image-title-wrap").append('<p id="'+fileName+'">'+value.name + '<button type="button" onclick="removeUpload(\''+fileName+'\')">Remove</button></p> <br><br>');
         });
       };
   
       reader.readAsDataURL(input.files[0]);
-  
-    } else {
-      removeUpload();
     }
   }
   
-  function removeUpload() {
-    $('.file-upload-input').replaceWith($('.file-upload-input').clone());
-    $('.file-upload-content').hide();
-    $('.image-upload-wrap').show();
+  $(function() {
+  $('#uploadData').click(function() {
+    
+      $.ajax({
+          type: 'POST',
+          url: '/sendfile',
+          data: form_data,
+          contentType: false,
+          cache: false,
+          processData: false,
+          success: function(data) {
+              console.log('Success!');
+          },
+      });
+  });
+  });
+  function removeUpload(id) {
+    $('#'+id).remove();
   }
   $('.image-upload-wrap').bind('dragover', function () {
           $('.image-upload-wrap').addClass('image-dropping');
